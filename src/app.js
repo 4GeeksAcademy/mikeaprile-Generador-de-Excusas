1af896755e290577b3201dd0bedfe412c75857fc
Michael Aprile Mancha <130480625+mikeaprilee@users.noreply.github.com>
2023-10-10 13:52:29 +0000

    Generador de Excusas terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#The-excuse").innerHTML = generateExcuse();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["The dog ", "My grandma ", "His turtle ", "My bird "];
+  let action = ["ate ", "peed ", "crushed ", "broke "];
+  let what = ["my homework ", "the keys ", "the car "];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    "" +
+    action[actionIndex] +
+    "" +
+    what[whatIndex] +
+    "" +
+    when[whenIndex]
+  );
 };
